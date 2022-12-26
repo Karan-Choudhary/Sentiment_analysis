dcfa04869e9f93e9905653148bbf8e339228d1b0
Karan Choudhary <kchoudhary510199@gmail.com>
2022-12-26 13:22:08 +0530

    fast api completed

--- BTsrc/app/inference.py
+++ BTsrc/app/inference.py
@@ -5,9 +5,13 @@ from app.prepareData import PrepareData
 
 # inference class
 class Inference(PrepareData):
+    '''
+    This class is used to make predictions on new data.
+    It inherits the PrepareData class.
+    '''
     def __init__(self):
         super().__init__()
-        self.model = load_model('saved_models/Final_model.h5')
+        self.model = load_model('app/saved_models/Final_model.h5')
         self.dataset = None
         self.X_final = None
         
--- BTsrc/app/main.py
+++ BTsrc/app/main.py
@@ -1,13 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
-# from mangum import Mangum
-# import uvicorn
 
 from app.inference import Inference
 
 app = FastAPI()
-# handler = Mangum(app)
 
 class TextIn(BaseModel):
     text: str
@@ -17,28 +14,19 @@ class PredictionOut(BaseModel):
 
 @app.post("/predict/{message}", response_model=PredictionOut)
 def predict(message: str):
-    # print the message
     message_list = [message]
-    # convert the message into a dataset
     inferenceClass = Inference()
     inferenceClass.convert_into_dataset(message_list)
-    # make predictions
+    
     y_pred = inferenceClass.predict()
     
-    # return the result
     
     if y_pred[0][0] == 0:
         sentiment = "Negative"
-        # return {"prediction": "Negative"}
         return {"sentiment": sentiment}
     else:
         sentiment = "Positive"
-        # return {"prediction": "Positive"}
         return {"sentiment": sentiment}
     
     sentiment = "Something went wrong"
     return {"sentiment": sentiment}
-    # return {"prediction": "Something went wrong"}
-    
-# if __name__ == "__main__":
-#     uvicorn.run(app, host="0.0.0.0", port=9000)
